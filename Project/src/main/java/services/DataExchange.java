295b438d5b3e804ca6e00b014b2e444fb33d9a71
Your Name <atenidkoyea@gmail.com>
2024-05-10 11:04:37 +0300

    added a class to retrieve data from mysql data base

--- Project/src/main/java/services/DataExchange.java
+++ Project/src/main/java/services/DataExchange.java
@@ -6,3 +6,17 @@ import java.sql.SQLException;
 
 import lejos.pc.comm.NXTCommLogListener;
 import lejos.pc.comm.NXTConnector;
+
+public class RobotDataHandler {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/robotdb";
+    private static final String USER = "root";
+    private static final String PASS = "Dherniza2!";
+
+    public static void main(String[] args) {
+        RobotDataHandler handler = new RobotDataHandler();
+        LejosData data = handler.retrieveDataFromDatabase();
+        if (data != null) {
+            handler.sendDataToRobot(data);
+        }
+    }
